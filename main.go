0c81ab407636fbe025eeb2e04bf74a5ed730a35f
HyunKyun Moon <mhg5303@gmail.com>
2025-04-14 14:43:53 +0900

    current

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
+func log(msg string) {
+	fmt.Println(msg)
+}
+
 func main() {
-	// This is the main function where the program starts executing.
-	// You can add your code here to perform specific tasks or call other functions.
-	fmt.Println("Hello, World!")
+	err := reconcile()
+	if err != nil {
+		log("Reconciler error " + err.Error()) // https://github.com/kubernetes-sigs/controller-runtime/blob/v0.18.4/pkg/internal/controller/controller.go#L324
+	}
+}
+
+func reconcile() error {
+	err := func1()
+	if err != nil {
+		log("Error in func1: " + err.Error())
+	}
+	return err
+}
+
+func func1() error {
+	err := func2()
+	if err != nil {
+		log("Error in func2: " + err.Error())
+	}
+	return err
+}
+
+func func2() error {
+	return errors.New("an error occurred in func2")
 }
