78760add50a1ac9ccf1c86f4385439fc2b65a327
HyunKyun Moon <mhg5303@gmail.com>
2025-04-14 14:42:08 +0900

    init

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// This is the main function where the program starts executing.
+	// You can add your code here to perform specific tasks or call other functions.
+	fmt.Println("Hello, World!")
+}
